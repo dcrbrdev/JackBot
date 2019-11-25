cf64f09a5a0205c59f4f935007f35b22ea79fbd9
rodrigondec <rodrigondec@gmail.com>
2019-11-24 22:19:30 -0300

    guilherme stuff removed

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,19 +1,3 @@
-from commands.base import config_handlers as base_handler
-from commands.callback import config_handlers as callback_handler
-from commands.error import config_handlers as error_handler
-from commands.message import config_handlers as message_handler
-from commands.sticker import config_handlers as sticker_handler
-from commands.coach import config_handlers as coach_handler
-from commands.weather import config_handlers as weather_handler
-from commands.house import config_handlers as house_handler
 
 handlers = [
-    base_handler,
-    callback_handler,
-    error_handler,
-    message_handler,
-    sticker_handler,
-    coach_handler,
-    weather_handler,
-    house_handler
 ]
