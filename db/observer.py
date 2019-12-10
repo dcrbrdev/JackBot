55275f626a320c011f93a8b772aa7fe5e7b80fcc
rodrigondec <rodrigondec@gmail.com>
2019-12-10 20:01:42 -0300

    ObserverMessage created

--- db/observer.py
+++ db/observer.py
@@ -1,9 +1,11 @@
 import logging
 
 from mongoengine import (
-    Document, EmbeddedDocument,
-    StringField,
-    ReferenceField, EmbeddedDocumentListField)
+    Document,
+    StringField, IntField, ListField,
+    ReferenceField, NULLIFY)
+from mongoengine.errors import DoesNotExist
+from telegram.error import BadRequest
 
 from bot.core import BotTelegramCore
 
@@ -15,37 +17,47 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-class ObserverMessage(EmbeddedDocument):
-    message_id = StringField(required=True)
+class ObserverMessage(Document):
+    message_id = IntField(required=True)
     subject = ReferenceField('Subject', required=True)
+
+    def __str__(self):
+        return f"{self.message_id} {self.subject}"
 
 
 class Observer(Document):
     username = StringField(max_length=50)
     chat_id = StringField(required=True, max_length=50, unique=True)
-    messages = EmbeddedDocumentListField(ObserverMessage)
-
-    meta = {
-        'indexes': [
-            {
-                'fields': ['messages.subject'],
-                'unique': True
-            }
-        ]
-    }
+    messages = ListField(
+        ReferenceField(ObserverMessage, reverse_delete_rule=NULLIFY)
+    )
 
     def __str__(self):
         return f"{self.username if self.username else ''} " \
                f"{self.chat_id}".strip()
 
     def notify(self, update_message):
+        self.reload()
         self._remove_last_message_from_subject(update_message.subject)
         self._send_update_message(update_message)
 
     def _send_update_message(self, update_message):
-        telegram_message = BotTelegramCore.instance().send_message(update_message, chat_id=self.chat_id)
-        self.messages.create(telegram_message.message_id, update_message.subject)
+        telegram_message = BotTelegramCore.send_message(f'{update_message}', chat_id=self.chat_id)
+        om = ObserverMessage(telegram_message.message_id, update_message.subject).save()
+        self.messages.append(om)
+        self.save()
 
     def _remove_last_message_from_subject(self, subject):
-        self.messages.get(subject=subject).delete()
-        self.save()
+        self.reload()
+        try:
+            for om in [om for om in self.messages if isinstance(om, ObserverMessage) and om.subject == subject]:
+                BotTelegramCore.delete_message(self.chat_id, om.message_id)
+                self.messages.remove(om)
+                self.save()
+                om.delete()
+        except DoesNotExist:
+            pass
+        except BadRequest as e:
+            logger.warning(f'BadRequest {e}')
+        except Exception as e:
+            logger.error(e)
