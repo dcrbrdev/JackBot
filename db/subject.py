52609da54ba30f8286b21787c8d31fc2a24225a9
rodrigondec <rodrigondec@gmail.com>
2019-12-11 20:31:13 -0300

    Created UserObserver

--- db/subject.py
+++ db/subject.py
@@ -7,7 +7,7 @@ from mongoengine import (
 
 from db.exceptions import (ObserverNotRegisteredError,
                            ObserverAlreadyRegisteredError)
-from db.observer import Observer
+from db.observer import Observer, UserObserver
 
 
 logging.basicConfig(
@@ -23,7 +23,7 @@ class Subject(Document):
     uri = StringField(required=True, max_length=120, unique=True)
 
     observers = ListField(
-        ReferenceField(Observer, reverse_delete_rule=NULLIFY), default=[]
+        ReferenceField(UserObserver, reverse_delete_rule=NULLIFY), default=[]
     )
 
     def __str__(self):
@@ -48,6 +48,9 @@ class Subject(Document):
         self.save()
 
     def notify(self, update_message):
+        oficial_observer = Observer.get_oficial_observer()
+        logger.info(f'Notifying oficial observer {oficial_observer} for {self}')
+        oficial_observer.notify(update_message)
         logger.info(f'Notifying observers {self.observers} for {self}')
         for observer in self.observers:
             observer.notify(update_message)
