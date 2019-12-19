f9f08f1992c67796ef3afe1c5160e5a44c0144b0
rodrigondec <rodrigondec@gmail.com>
2019-12-18 23:14:30 -0300

    tests ok

--- tests/db/test_session.py
+++ tests/db/test_session.py
@@ -23,6 +23,20 @@ class SessionTestCase(TestCase):
         self.assertEqual(instance.amounts[0].value, 10.0)
         self.assertEqual(instance.amounts[1].value, 20.0)
 
+    def test_equal(self):
+        instance = Session(hash='test',
+                           amounts=[Amount(1000000000), Amount(2000000000)])
+        other = Session(hash='test',
+                        amounts=[Amount(1000000000), Amount(2000000000)])
+        self.assertTrue(instance.equal(other))
+
+    def test_equal_false(self):
+        instance = Session(hash='test',
+                           amounts=[Amount(1000000000), Amount(2000000000)])
+        other = Session(hash='test',
+                        amounts=[Amount(1000000000), Amount(2500000000)])
+        self.assertFalse(instance.equal(other))
+
     def test_str(self):
         instance = Session(hash='test',
                            amounts=[Amount(1000000000), Amount(2000000000)])
--- tests/db/test_update_message.py
+++ tests/db/test_update_message.py
@@ -5,11 +5,16 @@ import pytest
 from tests.fixtures import mongo  # noqa F401
 from db.update_message import UpdateMessage, Session, Amount
 from db.subject import Subject
+from db.exceptions import DuplicatedUpdateMessageError
 
 
 DATA = '[{"name":"c17b1828e97bf66abd5329e7' \
        '3755173b43b98e18ebd4b84b19a016781d8cfa86",' \
        '"amounts":[1000000000,2000000000]}]\n'
+
+DATA2 = '[{"name":"c17b1828e97bf66abd5329e7' \
+       '3755173b43b98e18ebd4b84b19a016781d8cfa86",' \
+       '"amounts":[1000000000,2000000000,500000000]}]\n'
 
 
 @pytest.mark.usefixtures('mongo')
@@ -20,14 +25,36 @@ class UpdateMessageTestCase(TestCase):
                                ".com:8477/watchWaitingList").save()
 
     def test_init(self):
+        self.assertEqual(UpdateMessage.objects.count(), 0)
+
         UpdateMessage(self.subject, [Session('test', [Amount(10)])]).save()
+        self.assertEqual(UpdateMessage.objects.count(), 1)
+
         instance = UpdateMessage.objects.first()
         self.assertEqual(instance.subject, self.subject)
         self.assertIsInstance(instance, UpdateMessage)
 
-    def test_fromsessions(self):
-        data = DATA
-        UpdateMessage.from_data(self.subject, data)
+    def test_equal(self):
+        instance = UpdateMessage(self.subject, [Session('test', [Amount(10)])])
+        other = UpdateMessage(self.subject, [Session('test', [Amount(10)])])
+
+        self.assertTrue(instance.equal(other))
+
+    def test_equal_false(self):
+        instance = UpdateMessage(self.subject, [Session('test', [Amount(10)])])
+        other = UpdateMessage(self.subject, [Session('test', [Amount(11)])])
+
+        self.assertFalse(instance.equal(other))
+
+    def test_get_last_by_subject(self):
+        UpdateMessage(self.subject, [Session('test', [Amount(10)])]).save()
+        other = UpdateMessage(self.subject, [Session('test', [Amount(11)])]).save()
+
+        last = UpdateMessage.get_last_by_subject(self.subject)
+        self.assertEqual(other, last)
+
+    def test_from_sessions(self):
+        UpdateMessage.from_data(self.subject, DATA)
         instance = UpdateMessage.objects.first()
         self.assertEqual(instance.subject, self.subject)
         self.assertEqual(len(instance.sessions), 1)
@@ -42,3 +69,21 @@ class UpdateMessageTestCase(TestCase):
         self.assertIsInstance(session.amounts[1], Amount)
         self.assertEqual(session.amounts[0].value, 10.0)
         self.assertEqual(session.amounts[1].value, 20.0)
+
+    def test_from_sessions_duplicated(self):
+        self.assertEqual(UpdateMessage.objects.count(), 0)
+        UpdateMessage.from_data(self.subject, DATA)
+        self.assertEqual(UpdateMessage.objects.count(), 1)
+        self.assertRaises(DuplicatedUpdateMessageError, UpdateMessage.from_data, self.subject, DATA)
+        self.assertEqual(UpdateMessage.objects.count(), 1)
+
+    def test_from_sessions_2(self):
+        self.assertEqual(UpdateMessage.objects.count(), 0)
+        UpdateMessage.from_data(self.subject, DATA)
+        self.assertEqual(UpdateMessage.objects.count(), 1)
+        self.assertRaises(DuplicatedUpdateMessageError, UpdateMessage.from_data, self.subject, DATA)
+        self.assertEqual(UpdateMessage.objects.count(), 1)
+        UpdateMessage.from_data(self.subject, DATA2)
+        self.assertEqual(UpdateMessage.objects.count(), 2)
+        UpdateMessage.from_data(self.subject, DATA)
+        self.assertEqual(UpdateMessage.objects.count(), 3)
