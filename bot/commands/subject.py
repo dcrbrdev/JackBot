0017c37d80a66dd63f1c978ccc1a2583a9d6d1d5
rodrigondec <rodrigondec@gmail.com>
2020-01-16 15:41:27 -0300

    /now subject command on bot

---
+++ bot/commands/subject.py
@@ -0,0 +1,66 @@
+import logging
+
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
+from mongoengine.errors import DoesNotExist, MultipleObjectsReturned
+
+from bot.core import BotTelegramCore
+from utils.utils import convert_dcr
+from utils.exceptions import DcrDataAPIError
+from db.subject import Subject
+from db.observer import UserObserver
+from db.update_message import UpdateMessage
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def now(update: Update, context: CallbackContext):
+    chat = update.effective_chat
+    message = update.effective_message
+
+    try:
+        observer = UserObserver.objects.get(chat_id=f"{chat.id}")
+    except DoesNotExist:
+        observer = UserObserver(f"{chat.username}",
+                                f"{chat.id}").save()
+
+    subject_names = context.args
+    subjects = []
+    errors = []
+
+    if subject_names:
+        for name in subject_names:
+            try:
+                subject = Subject.objects.get(name__icontains=name)
+                subjects.append(subject)
+            except DoesNotExist as e:
+                errors.append(f"{name}: {e}")
+            except MultipleObjectsReturned as e:
+                errors.append(f"{e}\n"
+                              f"The value {name} returned more than one subject. "
+                              f"Please be more specific!")
+    else:
+        subjects = Subject.objects.all()
+
+    update_messages = []
+
+    for subject in subjects:
+        update_messages.append(UpdateMessage.get_last_by_subject(subject))
+
+    if errors:
+        error_message = "\n".join(errors)
+        message.reply_text(error_message)
+
+    for msg in update_messages:
+        observer.notify(msg)
+
+
+def config_handlers(instance: BotTelegramCore):
+    logger.info('Setting subject commands...')
+
+    instance.add_handler(CommandHandler("now", now))
