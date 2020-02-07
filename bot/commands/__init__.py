4a62f0f6c21ee9c162473bdbcfe832237e104cb2
rodrigondec <rodrigondec@gmail.com>
2020-02-07 17:15:57 -0300

    bot ticket commands created

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -3,11 +3,13 @@ from bot.commands.subscription import config_handlers as subscription_handlers
 from bot.commands.callback import config_handlers as callback_handlers
 from bot.commands.exchange import config_handlers as exchange_handlers
 from bot.commands.subject import config_handlers as subject_handlers
+from bot.commands.ticket import config_handlers as ticket_handlers
 
 handlers = [
     base_handlers,
     subscription_handlers,
     callback_handlers,
     exchange_handlers,
-    subject_handlers
+    subject_handlers,
+    ticket_handlers
 ]
